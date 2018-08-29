35556945c2bd67e2132a94fb267524774575c711
alonso <alonso.espinoza193@gmail.com>
2018-08-28 23:08:14 -0600

    ejercicio 14 listo

--- B22356-Ejercicios-1/14/ejercicio14.cpp
+++ B22356-Ejercicios-1/14/ejercicio14.cpp
@@ -3,11 +3,28 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    int numeroDecimal = atoi(argv[1]);
-    int dividendo = numeroDecimal;
+    int dividendo = atoi(argv[1]);
     int divisor = 2;
-    while (dividendo > divisor) {
-        
+    int modulo;
+    string binarioInverso = "";
+    string binario = "";
+    string temporal;
+    if (dividendo != 0) {
+        while (dividendo >= divisor) {
+            modulo = dividendo%divisor;
+            binarioInverso.append(to_string(modulo));
+            dividendo = dividendo/divisor;
+        }
+        binarioInverso.append("1");
+        for (int i = binarioInverso.length() - 1; i >= 0; i--) {
+            temporal = binarioInverso[i];
+            binario.append(temporal);
+        }
     }
+    else{
+        binario = "0";
+    }
+
+    cout << binario << endl;
     return 0;
 }
